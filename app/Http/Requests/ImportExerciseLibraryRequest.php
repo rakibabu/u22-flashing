a265bf26d7b3695cef6a2095828b5cffa346c5a8
Rakhal Imming <rakhalimming@gmail.com>
2026-08-17 15:13:53 +0200

    Fixed bug

--- app/Http/Requests/ImportExerciseLibraryRequest.php
+++ app/Http/Requests/ImportExerciseLibraryRequest.php
@@ -4,9 +4,9 @@ namespace App\Http\Requests;
 
 use App\Models\ExerciseLibraryItem;
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Validation\Validator;
 
 class ImportExerciseLibraryRequest extends FormRequest
 {
